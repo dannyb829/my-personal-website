b1a39953f2a07fff0391f9ee27953cafb14b2848
Daniel <dannyb829@gmail.com>
2022-02-07 00:19:00 -0500

    Added logo, Self Image, and cleaned up header

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-const h2 = document.createElement(`h2`);
 
-const h3 = document.createElement(`h3`)
-
-h2.textContent = `This content added by Javascript`;
-
-h3.textContent = `This was added with append to see where it goeas exactly`
-
-document.querySelector(`body`).append(h3)
-
-document.querySelector(`body`).appendChild(h2)
 
