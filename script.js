0725c9e39a1cd11e1bc7030fe9494921a2b42337
Daniel <dannyb829@gmail.com>
2022-02-23 20:40:05 -0500

    added user review prompt

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+setTimeout(userReview, 6000)
 
+let comment
+
+let commentList = []
+
+function userReview() {
+   comment = prompt("What do you think about my page?")
+   commentList.push(comment)
+   alert("Thanks for your thoughts!")
+}
 
